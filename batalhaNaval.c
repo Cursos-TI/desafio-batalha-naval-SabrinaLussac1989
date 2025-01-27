3ee4e77eb792ff2f6d516be4e45f6a66c3be9c57
SabrinaLussac1989 <pestilenceart@gmail.com>
2025-01-27 19:57:52 +0000

    REGISTRO DE NAVIO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,4 +2,12 @@
 int main (){
     printf("Desafio Xadrez \n");
     printf("Novo comit\n");
-}
+
+int matriz [10][10] = printf ("%d, matriz [X][Y]"); 
+
+   (char) ("NAVIOX") {"X= 0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n"};
+   (char) ("NAVIOY")  {"Y= 0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n"};
+        
+        
+        return 0;
+        }
